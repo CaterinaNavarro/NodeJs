1f49491bcd2c9ee8b4e77893df4e2d448f3b2d6d
Caterina Elizabeth Navarro <caterina.navarro7@gmail.com>
2021-07-15 21:45:09 -0300

    modulo02-tarea0102-validaciones

--- 02-practica node/app.js
+++ 02-practica node/app.js
@@ -1,6 +1,21 @@
 const CONSTANT_VALUE = 5;
 
-for (let i= 1; i <= 10 ; i++) 
+const generateTable = (number) =>
 {
-    console.log(`${CONSTANT_VALUE} x ${i} = ${CONSTANT_VALUE * i}`);
+    if (number === undefined)
+    {
+        throw new Error("Argument is missing.");
+    }
+
+    if (typeof(number) !== 'number')
+    {
+        throw new Error("Argument is not a number.");
+    }
+    
+    for (let i= 1; i <= 10 ; i++)
+    {
+        console.log(`${CONSTANT_VALUE} x ${i} = ${CONSTANT_VALUE * i}`);
+    }
 }
+
+generateTable(CONSTANT_VALUE);
