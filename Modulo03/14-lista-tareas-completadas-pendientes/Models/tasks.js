67b86909fd455f9726dc60d5aab6eeae0b281af7
Caterina Elizabeth Navarro <caterina.navarro7@gmail.com>
2021-08-01 20:09:31 -0300

    modulo03-tarea11

---
+++ Modulo03/14-lista-tareas-completadas-pendientes/Models/tasks.js
@@ -0,0 +1,45 @@
+import Task from './task.js'
+
+export default class Tasks {
+
+    list = null;
+
+    constructor()
+    {
+        this.list = {};
+    }
+
+    loadTasksFromArray = (arrTasks) => {
+        arrTasks.forEach(task => {
+            this.list[task.id] = task;
+        });
+    }
+
+    createTask = descrip => {
+        const task = new Task(descrip);
+        this.list[task.id] = task;
+    
+    }
+
+    listTasks = () => this.list;
+    
+    getArrList = () => {
+        const arrList = Object.entries(this.list).map(task => task[1]);
+        return arrList;
+    }
+
+    fullList = () => {
+        const arrList = this.getArrList();
+        const fullList = arrList.map(task => (
+            {
+                desc: task.desc, 
+                state: task.dateCompleted ? true : false
+            }
+        ));
+
+        return fullList;
+    }
+
+    listCompletedTasks = (completed = true) => this.fullList().filter(task => task.state === completed).map(task => task.desc);
+         
+}
